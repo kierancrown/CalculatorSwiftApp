0db751de2469ffc7931942ad0cc21b69cfc74a60
kierancrown <kieran.crown@gmail.com>
2022-02-01 20:31:35 +0000

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Kieran Crown on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
